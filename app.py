5c50063975500ddb60b80674ad5eb76cd93ed1bb
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-01-06 21:06:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import numpy as np
-import plotly.graph_objs as go
 
 st.title('cpanlp自然语言处理项目')
 st.header("Chart with two lines")
-trace0 = go.Scatter(x=[1, 2, 3, 4], y=[10, 15, 13, 17])
-trace1 = go.Scatter(x=[1, 2, 3, 4], y=[16, 5, 11, 9])
-data = [trace0, trace1]
-st.write(data)
+import matplotlib.pyplot as plt
+
+f = plt.figure()
+arr = np.random.normal(1, 1, size=100)
+plt.hist(arr, bins=20)
+
+st.plotly_chart(f)
