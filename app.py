666b2bb35a7cce6e069793863ec0fa78293dd11b
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-01-06 21:04:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title('cpanlp自然语言处理项目')
 st.header("Chart with two lines")
+trace0 = go.Scatter(x=[1, 2, 3, 4], y=[10, 15, 13, 17])
+trace1 = go.Scatter(x=[1, 2, 3, 4], y=[16, 5, 11, 9])
+data = [trace0, trace1]
+st.write(data)
