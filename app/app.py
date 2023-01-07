0edeae8110baeebec2e32dec99395dfd7e86ddb5
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-01-08 05:39:17 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,18 +4,18 @@ import pandas as pd
 import base64
 
 uploaded_file = st.file_uploader("上传csv文件", type="csv")
-data = [(1, 2, 3)]
-df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
+
+st.markdown(href, unsafe_allow_html=True)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+st.title('cpanlp自然语言处理项目')
+st.header("Chart with two lines")
+
 csv = df.to_csv(index=False)
 
 b64 = base64.b64encode(csv.encode()).decode()
-href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-st.markdown(href, unsafe_allow_html=True)
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data)
-st.title('cpanlp自然语言处理项目')
-st.header("Chart with two lines")
+href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (右击保存为.csv的文件 &lt;some_name&gt;.csv)'
 import matplotlib.pyplot as plt
 
 f = plt.figure()
