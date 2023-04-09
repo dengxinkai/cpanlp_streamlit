80aec575d5ab7c55f3cbf9d64bd5c84caebe62d4
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-09 17:55:06 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -44,7 +44,8 @@ def 分析(input_text):
     retriever = db.as_retriever()
     return RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever)
 qa = 分析(input_text)
-st.write(bb)
+if bb:
+    st.write(bb)
 
 input_text1 = st.text_input('查询', '')
 if st.button('查询'):
