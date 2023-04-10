1617d086770e683acd0d778828c1a42b43eb22f5
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-11 03:28:04 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -60,7 +60,12 @@ prompt_template1 = """Write a concise summary of the following:
 CONCISE SUMMARY IN CHINESE:"""
 PROMPT1 = PromptTemplate(template=prompt_template1, input_variables=["text"])
 result = ""
-template3 = """Answer the following questions as best you can.当问题与经济金融问题无关时，表明无法回答，因为与经济金融问题无关， You have access to the following tools:
+template3 = """Answer the following questions as best you can.当问题与经济金融问题无关时，
+Thought:表明无法回答，因为与经济金融问题无关
+Action: 无
+Action Input: 直接退出
+Observation:无
+You have access to the following tools:
 
 {tools}
 
