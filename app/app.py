553f1ddedd724910b9958a67400f08adda1595fe
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-14 10:46:36 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -170,7 +170,7 @@ class CustomOutputParser(AgentOutputParser):
         # Return the action and action input
         return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 output_parser = CustomOutputParser()
-st.header("上传")
+st.header(":blue[上传]")
 file = st.file_uploader("PDF文件", type="pdf")
 input_text = st.text_input('PDF网址', '')
 @st.cache(allow_output_mutation=True)
