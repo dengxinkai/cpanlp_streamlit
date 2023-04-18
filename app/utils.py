e7768140981cad9d0724cae99a34c18abf1b3a2b
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-19 03:39:17 +0800

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,15 @@
-template3 = """尽量以最少的iterations准确快速给出Final Answer,You have access to the following tools:
+template3 = """尽量以少的token准确快速给出Final Answer,You have access to the following tools:
     {tools}
     Use the following format:
     Question: the input question you must answer
-    Thought: you should always think about what to do,同时准确快速给出Final Answer
-    Action: the action to take, should be one of [{tool_names}],同时准确快速给出Final Answer
-    Action Input: the input to the action,同时准确快速给出Final Answer
-    Observation: the result of the action,同时准确快速给出Final Answer
-    Thought: I now know the final answer,同时准确快速给出Final Answer
+    Thought: you should always think about what to do,尽量少的token
+    Action: the action to take, should be one of [{tool_names}],尽量少的token
+    Action Input: the input to the action,尽量少的token
+    Observation: the result of the action,尽量少的token
+    Thought: I now know the final answer,尽量少的token
     Final Answer: the final answer to the original input question,准确快速给出Final Answer
-    All inputs and output tokens are limited to 3800.
+    All inputs 、output and context tokens are totally limited to 3800.
     Question: {input}
     {agent_scratchpad}
-    都用中文表示，除了格式中的Question:Thought:Action:Action Input:Observation:Thought:Final Answer:,总共处理时间不要超过15秒，总的context token不超过2000token。
-    be careful,model's maximum context length is 4000 tokens. messages must result in less than 4000 tokens
+    都用中文表示，除了格式中的Question:Thought:Action:Action Input:Observation:Thought:Final Answer
     """
