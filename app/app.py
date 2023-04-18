0f1e1689a54d2618e438f67d39eab1f165b63322
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-18 14:27:40 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -247,8 +247,8 @@ if st.session_state.input_api:
                 stop=["\nObservation:"], 
                 allowed_tools=tool_names
             )
-            agent_executor = AgentExecutor.from_agent_and_tools(agent=agent3, tools=tools, verbose=True,return_intermediate_steps=True)
-            response = agent_executor({"input":query})
+            agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True,return_intermediate_steps=True)
+            response = agent({"input":query})
             st.caption(response["output"])
             with st.expander("查看过程"):
                 st.write(response["intermediate_steps"])
