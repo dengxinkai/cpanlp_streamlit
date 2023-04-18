195b4059bf3ee898496e8dc5666fc5e3a84a681c
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-19 03:14:50 +0800

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,20 @@
 
+def 中国平安年报查询(input_text):
+    pinecone.init(api_key="bd20d2c3-f100-4d24-954b-c17928d1c2da",  # find at app.pinecone.io
+                      environment="us-east4-gcp",  # next to api key in console
+                      namespace="ZGPA_601318")
+    index = pinecone.Index(index_name="kedu")
+    a=embeddings.embed_query(input_text)
+    www=index.query(vector=a, top_k=1, namespace='ZGPA_601318', include_metadata=True)
+    c = [x["metadata"]["text"] for x in www["matches"]]
+    return c
+def 双汇发展年报查询(input_text):
+    namespace="ShHFZ_000895"
+    pinecone.init(api_key="bd20d2c3-f100-4d24-954b-c17928d1c2da",  # find at app.pinecone.io
+                      environment="us-east4-gcp",  # next to api key in console
+                      namespace=namespace)
+    index = pinecone.Index(index_name="kedu")
+    a=embeddings.embed_query(input_text)
+    www=index.query(vector=a, top_k=1, namespace=namespace, include_metadata=True)
+    c = [x["metadata"]["text"] for x in www["matches"]]
+    return c
