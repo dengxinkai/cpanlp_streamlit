0e8a9171f25ec6a72c0fb3244f5a0964fdb5d263
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-19 03:23:12 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ from langchain.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
-from utils import template3
+from utils import template3,中国平安年报查询,双汇发展年报查询
 st.set_page_config(
     page_title="可读-财报GPT",
     page_icon="https://raw.githubusercontent.com/dengxinkai/cpanlp_streamlit/main/app/%E6%9C%AA%E5%91%BD%E5%90%8D.png",
