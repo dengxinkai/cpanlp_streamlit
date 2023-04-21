28cce4c9fbe8cc3c89c549a8a82054efd4664fcb
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-21 18:44:40 +0800

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -379,11 +379,11 @@ with st.expander("数字人生成"):
        submitted = st.form_submit_button("生成数字人")
        if submitted:
             st.write("当前存在的数字人：")
-
+d = {} 
             
 agent_name = f"{name}_{age}"
-st.session_state[agent_name] = GenerativeAgent(name=name, age=age, traits=traits,status=status,memory_retriever=create_new_memory_retriever(),llm=LLM,daily_summaries = [("杨丹是邓新凯的博士生导师，他是北京外国语大学的校长，平时比较忙")],reflection_threshold = reflection_threshold,)           
-for agent_name, agent in st.session_state.items():
+d[agent_name] = GenerativeAgent(name=name, age=age, traits=traits,status=status,memory_retriever=create_new_memory_retriever(),llm=LLM,daily_summaries = [("杨丹是邓新凯的博士生导师，他是北京外国语大学的校长，平时比较忙")],reflection_threshold = reflection_threshold,)           
+for agent_name, agent in d.session_state.items():
     if isinstance(agent, GenerativeAgent):
         st.header(agent_name)
         st.header(agent.name)
