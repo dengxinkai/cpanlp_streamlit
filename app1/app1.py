c037758ac1346d2b5ac7b4acdffb5394040673db
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-22 17:02:40 +0800

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -369,7 +369,22 @@ def create_new_memory_retriever():
     index = faiss.IndexFlatL2(embedding_size)
     vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {}, relevance_score_fn=relevance_score_fn)
     return TimeWeightedVectorStoreRetriever(vectorstore=vectorstore, other_score_keys=["importance"], k=15)  
-
+def run_conversation(agents: List[GenerativeAgent], initial_observation: str) -> None:
+    """Runs a conversation between agents."""
+    _, observation = agents[1].generate_reaction(initial_observation)
+    print(observation)
+    turns = 0
+    while True:
+        break_dialogue = False
+        for agent in agents:
+            stay_in_dialogue, observation = agent.generate_dialogue_response(observation)
+            print(observation)
+            # observation = f"{agent.name} said {reaction}"
+            if not stay_in_dialogue:
+                break_dialogue = True   
+        if break_dialogue:
+            break
+        turns += 1
 with st.form("my_form"):
     global agent1
     col1, col2 = st.columns(2)
