1ef96e57eb52ff3acd64cc053a9b7ff288df733b
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-22 16:35:15 +0800

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -67,6 +67,7 @@ with st.sidebar:
 st.title('数字人')
 USER_NAME = "Person A" # The name you want to use when interviewing the agent.
 LLM = ChatOpenAI(max_tokens=1500) # Can be any LLM you want.
+global agents
 agents={}
 class GenerativeAgent(BaseModel):
     name: str
