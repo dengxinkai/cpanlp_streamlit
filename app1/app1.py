e01289ee032abf16b58d2f1a31403eecb64b6dc4
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-24 03:00:53 +0800

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -68,8 +68,9 @@ with st.sidebar:
 #         st.text_input(st.session_state["input_api"], key="input_api",label_visibility="collapsed")
         pass
     else:
-        st.info('请先输入正确的openai api-key')
-        st.text_input('api-key','', key="input_api")
+        pass
+#         st.info('请先输入正确的openai api-key')
+#         st.text_input('api-key','', key="input_api")
     with st.expander("ChatOpenAI属性设置"):
         temperature = st.slider("`temperature`", 0.01, 0.99, 0.3,help="用于控制生成文本随机性和多样性的参数。较高的温度值通常适用于生成较为自由流畅的文本，而较低的温度值则适用于生成更加确定性的文本。")
         frequency_penalty = st.slider("`frequency_penalty`", 0.01, 0.99, 0.3,help="用于控制生成文本中单词重复频率的技术。数值越大，模型对单词重复使用的惩罚就越严格，生成文本中出现相同单词的概率就越低；数值越小，生成文本中出现相同单词的概率就越高。")
