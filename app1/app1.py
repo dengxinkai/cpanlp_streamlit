70f0ce81cbcc8b87e2e87e2d2fa97b287bf42e1c
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-24 01:45:18 +0800

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -53,12 +53,12 @@ st.set_page_config(
 )
 # 从本地文件中读取会话状态
 def load_session_state():
-    with open("session_state.pickle", "rb") as f:
+    with open("/Users/dengxinkaiacca163.com/Desktop/语言学理论/react1/session_state.pickle", "rb") as f:
         return pickle.load(f)
 
 # 将会话状态保存到本地文件
 def save_session_state(session_state):
-    with open("session_state.pickle", "wb") as f:
+    with open("/Users/dengxinkaiacca163.com/Desktop/语言学理论/react1/session_state.pickle", "wb") as f:
         pickle.dump(session_state, f)
 with st.sidebar:
     if 'input_api' in st.session_state:
