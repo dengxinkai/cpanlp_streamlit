ed78c534594a334ece462c4b3cd74ea3e8d744b2
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-25 17:40:40 +0800

    Update 数字人.py

--- app1/数字人.py
+++ app1/数字人.py
@@ -67,7 +67,7 @@ with st.sidebar:
                                 "gpt-4"),
                                 index=0)
     
-    USER_NAME = st.text_input(st.session_state["user_name"], key="user_name",label_visibility="collapsed")
+    USER_NAME = st.text_input("请输入你的名字","", key="user_name",label_visibility="collapsed")
     st.write(f"你好 {USER_NAME}")
 
 agent_keys = [key for key in st.session_state.keys() if key.startswith('agent')]   
