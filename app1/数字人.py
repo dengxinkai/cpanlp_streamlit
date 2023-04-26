9b4fb6a05ef29056ec22040c27005fc27961ef4a
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-27 02:26:16 +0800

    Update 数字人.py

--- app1/数字人.py
+++ app1/数字人.py
@@ -491,7 +491,7 @@ with tab1:
                 agent1.add_memory(memory)    
             st.session_state[f"agent_{name}"] = agent1
             st.experimental_rerun()
-    uploaded_file = st.file_uploader("通过csv文件批量建立数字人", type=["csv"],help="csv格式：姓名、年龄、性别、特征、状态、反思阈值、记忆、总结")
+    uploaded_file = st.file_uploader("csv文件上传批量建立", type=["csv"],help="csv格式：姓名、年龄、性别、特征、状态、反思阈值、记忆、总结")
     if uploaded_file is not None:
         data = pd.read_csv(uploaded_file)
         st.dataframe(data)
