a19bba3d0750edd02248347e71f866e66090111e
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-27 15:33:20 +0800

    Update app4.py

--- app4/app4.py
+++ app4/app4.py
@@ -36,7 +36,6 @@ from langchain.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
 )
-from utils import template3
 
 st.set_page_config(
     page_title="可读-财报GPT",
@@ -224,6 +223,21 @@ class BabyAGI(Chain, BaseModel):
             **kwargs
         )
 wikipedia.set_lang("zh")
+template3 = """尽量以少的token准确快速给出Final Answer,You have access to the following tools:
+    {tools}
+    Use the following format:
+    Question: the input question you must answer
+    Thought: you should always think about what to do,尽量少的token
+    Action: the action to take, should be one of [{tool_names}],尽量少的token
+    Action Input: the input to the action,尽量少的token
+    Observation: the result of the action,尽量少的token
+    Thought: I now know the final answer,尽量少的token
+    Final Answer: the final answer to the original input question,准确快速给出Final Answer
+    All inputs 、output and context tokens are totally limited to 3800.
+    Question: {input}
+    {agent_scratchpad}
+    都用中文表示，除了格式中的Question:Thought:Action:Action Input:Observation:Thought:Final Answer
+    """
 with st.sidebar:
     st.info('请先输入正确的openai api-key')
     st.text_input('api-key','', key="input_api")
