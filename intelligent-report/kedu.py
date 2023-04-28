a7551b156c0fb6bc6e869692e184a0f282ad705e
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-28 15:32:34 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -430,7 +430,7 @@ if st.session_state.input_api:
                     wwww= RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
                 if st.button('确认',key="file"):
                     start_time = time.time()
-                    st.success(wwww.run("公司情况"))
+                    st.success(wwww.run(input_file))
                     end_time = time.time()
                     elapsed_time = end_time - start_time
                     with st.expander("费用"):
@@ -461,9 +461,11 @@ if st.session_state.input_api:
                 db = Chroma.from_documents(texts, embeddings_cho)
                 retriever = db.as_retriever()
                 wwww= RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, chain_type_kwargs=chain_type_kwargs)
+                input_file_web = st.text_input('查询内容','',key="input_file_web")
+
                 if st.button('确认',key="fileweb"):
                     start_time = time.time()
-                    st.success(wwww.run("公司情况"))
+                    st.success(wwww.run(input_file_web))
                     end_time = time.time()
                     elapsed_time = end_time - start_time
                     with st.expander("费用"):
