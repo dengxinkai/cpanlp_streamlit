d443f38d6b7437de104958f833ce022b41aeb302
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-28 21:48:11 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -1,35 +1,19 @@
 import streamlit as st
 import asyncio
-import faiss
 import numpy as np
 import pandas as pd
-import base64
-import json
-import os
 import tempfile
-import pinecone 
-import requests
 import re
 import time
 from typing import List, Union,Callable,Dict, Optional, Any
-from langchain.prompts import StringPromptTemplate,PromptTemplate
-from langchain import OpenAI, SerpAPIWrapper, LLMChain
-from langchain.schema import AgentAction, AgentFinish,Document
+from langchain.prompts import PromptTemplate
 from langchain.document_loaders import PyPDFLoader
-from langchain.docstore import InMemoryDocstore
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
-from langchain.chains.base import Chain
-from langchain.llms import OpenAI,BaseLLM
 from langchain.embeddings import OpenAIEmbeddings,HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma,FAISS
-from langchain.vectorstores.base import VectorStore
+from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-)
 from utils import template3
 st.set_page_config(
     page_title="智能财报",
