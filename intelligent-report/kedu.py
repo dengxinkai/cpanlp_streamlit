0e5be54bfcde4bbef8b031813aeb6a7beabe5ea1
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-29 00:08:06 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -53,7 +53,7 @@ with st.sidebar:
 @st.cache_data(persist="disk")
 def convert_df(df):
    return df.to_csv(index=False).encode('utf-8')
-@st.cache_data(persist="disk")
+@st.cache_resource
 def upload_file(input_text):
     loader = PyPDFLoader(input_text)
     prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.{context}Question: {question}Answer in Chinese:"""
