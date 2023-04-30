4414cd919356aab03b4746c9dd715aff2c8b7039
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-30 21:12:59 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -61,7 +61,7 @@ with st.sidebar:
 @st.cache_data(persist="disk")
 def convert_df(df):
    return df.to_csv(index=False).encode('utf-8')
-st.info("🫡 :red[记得经常使用刷新和清除缓存功能]")
+st.warning("🫡 :red[记得经常使用刷新和清除缓存功能]")
 if st.button('刷新页面',key="rerun"):
     st.experimental_rerun()
 if st.button('清除所有缓存',key="clearcache"):
@@ -75,7 +75,7 @@ pinecone.init(api_key="1ebbc1a4-f41e-43a7-b91e-24c03ebf0114",  # find at app.pin
                       namespace=pinename
                       )
 index = pinecone.Index(index_name="kedu")
-st.write("🫡 :red[别忘了删除不再使用的数据库]")
+st.warning("🫡 :red[别忘了删除不再使用的数据库]")
 if st.button('删除数据库',key="deletepine"):
     index = pinecone.Index(index_name="kedu")
     index.delete(deleteAll='true', namespace=pinename)
@@ -134,7 +134,7 @@ if st.session_state.input_api:
         if fileoption=="本地上传":
             file = st.file_uploader("PDF上传", type="pdf",key="upload")
             input_file = st.text_input('**查询**','公司核心竞争力',key="file_web",help="例子")
-            st.write("🫡 :red[使用数据库查询只需要通过 API 接口获取嵌入向量，而不需要进行其他 API 调用，但使用 AI 查询需要使用 API 接口，并且会产生一定费用。]")
+            st.warning("🫡 :red[使用数据库查询只需要通过 API 接口获取嵌入向量，而不需要进行其他 API 调用，但使用 AI 查询需要使用 API 接口，并且会产生一定费用。]")
             if st.button('数据库查询',key="file_upload"):
                 ww=upload_query(input_file)
                 st.success(ww)
