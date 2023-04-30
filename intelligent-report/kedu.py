b26a8475d1522b628460c342c0ec08bb8a937f01
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-30 14:49:13 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -32,7 +32,7 @@ st.set_page_config(
 logo_url = "https://raw.githubusercontent.com/dengxinkai/cpanlp_streamlit/main/app/%E6%9C%AA%E5%91%BD%E5%90%8D.png"
 with st.sidebar:
     st.image(logo_url,width=150)
-    st.subheader("➡️第一步：输入api")
+    st.subheader("➡️:blue[第一步：输入api]")
     if 'input_api' in st.session_state:
         st.text_input("api-key",st.session_state["input_api"], key="input_api")
     else:
@@ -65,6 +65,7 @@ if st.button('刷新页面',key="rerun"):
 if st.button('清除所有缓存',key="clearcache"):
     st.cache_data.clear()
 
+st.subheader("➡️:blue[第二步：取一个数据库名称]")
 pinename = st.text_input('**数据库名称**','example',key="pinename",help="请注意，系统每日定期清除数据库")
 
 pinecone.init(api_key="1ebbc1a4-f41e-43a7-b91e-24c03ebf0114",  # find at app.pinecone.io
