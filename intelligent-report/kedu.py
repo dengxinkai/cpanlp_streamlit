eb8f7b29c7d8dff76b022ce4300765fa88c88df4
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-04-30 20:27:31 +0800

    Update kedu.py

--- intelligent-report/kedu.py
+++ intelligent-report/kedu.py
@@ -68,7 +68,7 @@ if st.button('清除所有缓存',key="clearcache"):
     st.cache_data.clear()
 
 st.subheader("👇 :blue[第二步：取一个独特的数据库名称]")
-pinename = st.text_input('**数据库名称**','example',key="pinename",help="请注意，系统每日定期清除数据库")
+pinename = st.text_input('**数据库名称**','report',key="pinename",help="请注意，系统每日定期清除数据库")
 
 pinecone.init(api_key="1ebbc1a4-f41e-43a7-b91e-24c03ebf0114",  # find at app.pinecone.io
                       environment="us-west1-gcp-free", 
