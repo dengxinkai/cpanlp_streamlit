b9bd95e58ad7ae39c2869b7fad2a9ef6f77f597f
nlp-swiftui <78074644+dengxinkai@users.noreply.github.com>
2023-05-10 00:46:46 +0800

    Update 数字人.py

--- app1/数字人.py
+++ app1/数字人.py
@@ -40,7 +40,7 @@ with st.sidebar:
             st.text_input(st.session_state["input_api"], key="input_api",label_visibility="collapsed")
         else:
             st.info('请先输入正确的openai api-key')
-            st.text_input('api-key','', key="input_api")
+            st.text_input('api-key','', key="input_api",type="password")
         temperature = st.slider("`temperature`", 0.01, 0.99, 0.3,help="用于控制生成文本随机性和多样性的参数。较高的温度值通常适用于生成较为自由流畅的文本，而较低的温度值则适用于生成更加确定性的文本。")
         frequency_penalty = st.slider("`frequency_penalty`", 0.01, 0.99, 0.3,help="用于控制生成文本中单词重复频率的技术。数值越大，模型对单词重复使用的惩罚就越严格，生成文本中出现相同单词的概率就越低；数值越小，生成文本中出现相同单词的概率就越高。")
         presence_penalty = st.slider("`presence_penalty`", 0.01, 0.99, 0.3,help="用于控制语言生成模型生成文本时对输入提示的重视程度的参数。presence_penalty的值较低，模型生成的文本可能与输入提示非常接近，但缺乏创意或原创性。presence_penalty设置为较高的值，模型可能生成更多样化、更具原创性但与输入提示较远的文本。")
